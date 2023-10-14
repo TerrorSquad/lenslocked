e5c860e48dfc6a3a47cb54ee095683cac049e446
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-14 20:59:15 +0200

    Refactor handlers and add contactHandler
    - Refactored the handlerFunc to homeHandler for better clarity
    - Added a new contactHandler to handle requests to "/contact"
    - Set the "Content-Type" header to "text/html" in both handlers

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>To get in touch, please send an email to <a href=\"mailto:test@example.com\">John Doe</a>.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
