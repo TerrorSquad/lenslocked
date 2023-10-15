f5c6ad037d8313f28989eb49ba980653f3f027cf
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-15 15:57:59 +0200

    Refactor code to add FAQ page handler
    - Added `faqHandler` function to handle requests for the FAQ page
    - Updated `Router` struct to implement the `ServeHTTP` method
    - Modified switch statement in `ServeHTTP` method to include case for "/faq"
    - Removed commented out code and unused imports

--- main.go
+++ main.go
@@ -15,7 +15,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>To get in touch, please send an email to <a href=\"mailto:test@example.com\">John Doe</a>.</p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>FAQ page</p>")
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -23,38 +30,14 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 		break
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.NotFound(w, r)
 	}
 }
 
-type Router struct{}
-
-//func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	switch r.URL.Path {
-//	case "/":
-//		homeHandler(w, r)
-//		break
-//	case "/contact":
-//		contactHandler(w, r)
-//		break
-//	default:
-//		http.NotFound(w, r)
-//	}
-//}
-
 func main() {
+	http.ListenAndServe("localhost:3000", Router{})
 	fmt.Println("Server is running on port 3000")
-
-	//// First way: store the handler in a variable
-	//// long form
-	//var router http.HandlerFunc = pathHandler
-	//// short form
-	//router := http.HandlerFunc(pathHandler)
-	//// pass the router to the ListenAndServe function
-	//http.ListenAndServe("localhost:3000", router)
-
-	// Second way: use http.HandlerFunc as a type conversion directly.
-	// Second argument is not a function call, but a conversion, similar to (int) 42 in C
-	http.ListenAndServe("localhost:3000", http.HandlerFunc(pathHandler))
 }
