18cf88f2157697779e7ca926598c131bf156904f
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-15 19:05:39 +0200

    Add hello.gohtml template and main.go file
    - Added a new file `hello.gohtml` which contains an HTML template for displaying user information.
    - Added a new file `main.go` which includes the main function to execute the template and print user details on the console.

---
+++ cmd/exp/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "John Doe",
+		Age:  111,
+		Meta: UserMeta{
+			Visits: 42,
+		},
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+
+}
