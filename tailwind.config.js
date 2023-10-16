708c34e0a65edb0da5c31c91938fef7ecb502969
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-16 20:17:15 +0200

    feat: Add Tailwind CSS support and update templates
    - Added `tailwindcss` to `.gitignore`
    - Created `app.css` with Tailwind CSS configuration
    - Updated `modd.conf` to watch for changes in `app.css` and compile it to `dist/app.css`
    - Added `postcss.config.js` with Tailwind CSS and Autoprefixer plugins
    - Added `tailwind.config.js` with basic configuration
    - Updated `home.gohtml` template to include a styled heading
    - Updated `layout-page-tailwind.gohtml` template to include the compiled CSS file and an empty heading

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{gohtml,js}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
