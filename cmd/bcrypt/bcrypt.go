e6d651f923d09fd5af7efabd5f4ebf1241f125ac
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-20 13:12:15 +0200

    feat: Add bcrypt dependancy and add bcrypt functionality in a separate experimental file
    This commit adds the bcrypt functionality to the codebase, allowing for password hashing and comparison.

---
+++ cmd/bcrypt/bcrypt.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+func main() {
+	//for i, arg := range os.Args {
+	//	fmt.Println(i, arg)
+	//}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: bcrypt <hash|compare> <password> [<hash>]")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "hash":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: bcrypt hash <password>")
+			os.Exit(1)
+		}
+		hash(os.Args[2])
+	case "compare":
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: bcrypt compare <password> <hash>")
+			os.Exit(1)
+		}
+		compare(os.Args[2], os.Args[3])
+	default:
+		fmt.Println("Usage: bcrypt <hash|compare> <password> [<hash>]")
+	}
+}
+
+func hash(password string) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("Error hashing:", password, err)
+	}
+	fmt.Println("Hashed password:", string(hashedBytes))
+}
+
+func compare(password, hash string) {
+	fmt.Println("TODO: Compare the password:", password, hash)
+}
--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/terrorsquad/lenslocked
 
 go 1.21.3
 
+require github.com/go-chi/chi/v5 v5.0.10
+
 require (
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -12,6 +13,6 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v4 v4.18.1 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
