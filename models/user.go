d5d99f7ff7a04f800d11b4574abf2aa1c9239a57
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-20 14:35:32 +0200

    feat: Add user creation functionality
    This commit adds the ability to create a new user. It introduces a `NewUser` struct with email and password fields, and a `Create` method in the `UserService` that takes an email and password as parameters. The method converts the email to lowercase, hashes the password using bcrypt, and inserts the user into the database.

--- models/user.go
+++ models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+)
 
 type User struct {
 	ID           uint
@@ -10,4 +15,30 @@ type User struct {
 
 type UserService struct {
 	DB *sql.DB
+}
+
+type NewUser struct {
+	email    string
+	password string
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	passwordHash := string(hashedBytes)
+
+	user := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+	row := us.DB.QueryRow(`INSERT INTO users (name, email) VALUES ($1, $2);`, email, passwordHash)
+	err = row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	return &user, nil
 }
