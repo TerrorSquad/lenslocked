590405d52f0c97df76ad903549879fdb042117bb
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-10-22 19:12:26 +0200

    Refactor Session struct and add SessionService methods
    - Refactored the Session struct to change ID and UserID types from uint to int
    - Added Token field to the Session struct, which is only set when creating a new session
    - Added Create method to the SessionService struct for creating a new session (TODO: implementation pending)
    - Added User method to the SessionService struct for retrieving user information based on a session token (TODO: implementation pending)

--- models/session.go
+++ models/session.go
@@ -1,7 +1,27 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        uint
-	UserID    uint
+	ID     int
+	UserID int
+	// Token is only set when creating a new session. When looking up a session
+	// this will be empty, as we only store the hashed version of the token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: 1. Create the session token
+	// TODO: 2. Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
