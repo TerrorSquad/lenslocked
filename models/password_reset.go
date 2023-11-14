9acbe4c29a8eaac437096faa1c6ed4049ab72699
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-14 17:23:35 +0100

    feat: Implement Create method in PasswordResetService
    This commit adds the implementation for the Create method in the PasswordResetService. It retrieves the user ID based on the provided email, generates a token, and inserts a new password reset record into the database. The token is hashed using SHA256 and encoded with base64.

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,8 +1,12 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"github.com/terrorsquad/lenslocked/rand"
+	"strings"
 	"time"
 )
 
@@ -31,9 +35,54 @@ type PasswordResetService struct {
 }
 
 func (service *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: implement PasswordResetService.Create()")
+	email = strings.ToLower(email)
+	var userId uint
+	row := service.DB.QueryRow(`SELECT id FROM users WHERE email = $1;`, email)
+	err := row.Scan(&userId)
+	if err != nil {
+		// TODO: Consider returning a specific error if the user does not exist.
+		return nil, fmt.Errorf("create password reset: %w", err)
+	}
+	bytesPerToken := service.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	duration := service.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+	passwordReset := PasswordReset{
+		UserID:    userId,
+		Token:     token,
+		TokenHash: service.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	row = service.DB.QueryRow(`
+			INSERT INTO password_resets (user_id, token_hash, expires_at)
+			VALUES ($1, $2, $3) ON CONFLICT (user_id) DO
+			UPDATE SET token_hash = $2 RETURNING id;`,
+		passwordReset.UserID, passwordReset.TokenHash, passwordReset.ExpiresAt,
+	)
+	err = row.Scan(&passwordReset.ID)
+
+	if err != nil {
+		return nil, fmt.Errorf("create password reset: %w", err)
+	}
+
+	return &passwordReset, nil
+
 }
 
 func (service *PasswordResetService) Consume(token string) (*PasswordReset, error) {
 	return nil, fmt.Errorf("TODO: implement PasswordResetService.Consume()")
+}
+
+func (service *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
