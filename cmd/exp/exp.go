0894567bc1ff37ff3e05b1bdb1111be094ed4b8f
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-14 01:39:30 +0100

    feat: Add SMTP client configuration and send mail
    - Added constants for port, host, username, and password
    - Configured SMTP client with provided credentials
    - Sent a test email using the configured client

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,7 +3,13 @@ package main
 import (
 	"github.com/wneessen/go-mail"
 	"log"
-	"os"
+)
+
+const (
+	port     = 2525
+	host     = "hhhhhhhhhh"
+	username = "uuuuuuuuuuu"
+	password = "ppppppppppp"
 )
 
 func main() {
@@ -16,6 +22,17 @@ func main() {
 	}
 	m.Subject("This is my first mail with go-mail!")
 	m.SetBodyString(mail.TypeTextPlain, "Do you like this mail? I certainly do!")
-
-	m.WriteTo(os.Stdout)
+	c, err := mail.NewClient(
+		host,
+		mail.WithPort(port),
+		mail.WithSMTPAuth(mail.SMTPAuthCramMD5),
+		mail.WithUsername(username),
+		mail.WithPassword(password),
+	)
+	if err != nil {
+		log.Fatalf("failed to create mail client: %s", err)
+	}
+	if err := c.Send(m); err != nil {
+		log.Fatalf("failed to send mail: %s", err)
+	}
 }
