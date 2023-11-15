c369b8970d9dd1375e7823e3d57960a9bfc5e9ee
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-15 16:00:03 +0100

    feat: Add Create method to GalleryService
    This commit adds a new method called Create to the GalleryService struct. The Create method takes in a userID and title as parameters, creates a new Gallery instance with the provided values, and inserts it into the galleries table in the database using an SQL query. It then returns the created gallery along with any error encountered during the process.

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,26 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	ID     uint
 	UserID uint
 	title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (service *GalleryService) Create(userID uint, title string) (*Gallery, error) {
+	gallery := Gallery{UserID: userID, title: title}
+	row := service.DB.QueryRow(`INSERT INTO galleries (user_id, title) VALUES ($1, $2) RETURNING id;`, gallery.UserID, gallery.title)
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+	return &gallery, nil
 }
