334968ca723501b2a69c4846b033a1bf66f6941c
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-21 23:45:24 +0100

    Update storage init script to create a symlink between images and /data/storage/images

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env sh
-FOLDER=/app
-# This script should open the /app directory
-# Then it should check if the /app/storage directory exists
-# If it does not exist, it should create it by making it a symlink to /data/storage
-# Also create the /app/images directory if it does not exist
-# It should be a link to /data/images
+FOLDER=/data/storage/images
 
 if [ ! -d "$FOLDER" ]; then
-  echo "Hey";
+    mkdir -p /data/storage/images
+    ln -s /app/images /data/storage/images
 fi
