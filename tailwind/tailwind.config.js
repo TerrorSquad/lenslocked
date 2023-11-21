36de01c2889c9fb5fd520aefcae73bd860ab127f
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-21 16:50:51 +0100

    Build tailwind locally

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+    content: ["../templates/**/*.{gohtml,html}"],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
