40890b445ca09a7e01c44b7b95476646da34a984
Goran Ninkovic <g.ninkovic@angeltech.rs>
2023-11-21 01:22:30 +0100

    Refactor error handling and add file validation functions
    - Refactored error handling in the models package
    - Added a new FileError type to handle file-related errors
    - Implemented checkContentType function to validate file content type
    - Implemented checkExtension function to validate file extension

--- models/errors.go
+++ models/errors.go
@@ -1,8 +1,48 @@
 package models
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
+)
 
 var (
 	ErrEmailTaken = errors.New("models: email address is already taken")
 	ErrNotFound   = errors.New("models: resource not found")
 )
+
+type FileError struct {
+	Message string
+}
+
+func (fe FileError) Error() string {
+	return fmt.Sprintf("Invalid file: %v", fe.Message)
+}
+
+func checkContentType(r io.ReadSeeker, allowedTypes []string) error {
+	testBytes := make([]byte, 512)
+	_, err := r.Read(testBytes)
+	if err != nil {
+		return fmt.Errorf("reading file: %w", err)
+	}
+	_, err = r.Seek(0, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("seeking file: %w", err)
+	}
+	contentType := http.DetectContentType(testBytes)
+	for _, allowedType := range allowedTypes {
+		if contentType == allowedType {
+			return nil
+		}
+	}
+	return FileError{Message: fmt.Sprintf("invalid content type: %v", contentType)}
+}
+
+func checkExtension(filename string, allowedExtensions []string) error {
+	if !hasExtension(filename, allowedExtensions) {
+		return FileError{Message: fmt.Sprintf("invalid extension: %v", filepath.Ext(filename))}
+	}
+	return nil
+}
